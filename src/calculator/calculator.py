8afb9a68f546789cdb9d41b5c8080affc34dba6f
Sansar Kharal <112936761+kharsans@users.noreply.github.com>
2024-10-23 18:16:11 -0600

    Update calculator.py
    I have added 3 calculators in the file

--- src/calculator/calculator.py
+++ src/calculator/calculator.py
@@ -1,5 +1,6 @@
 """This module contains the calculator functions for the formulas square, tri, lazy caterer, and magic squares"""
 
+import math 
 
 def squareNums(n):
     """Calculates the square"""
@@ -20,6 +21,21 @@ def magicSquares(n):
     """Calculates the magic squares number"""
     return (n * (n**2 + 1)) / 2
 
+"""my three calculator are here after this comment"""
+
+def calculatehypotenuse(a, b):
+    """Calculates hypotenuse"""
+    return math.sqrt(a**2 + b**2)
+
+
+def volumeOfSphere(radius):
+    """Calculates volume of sphere when """
+    return (4/3) * math.pi * (radius ** 3)
+    
+    
+def celsiusToFahrenheit(celsius):
+    return (celsius * 9/5) + 32
+    
 
 def run_calculator(input_formula, input_num):
     """Calls and returns results for the specified formulas"""
@@ -31,7 +47,7 @@ def run_calculator(input_formula, input_num):
 
 if __name__ == "__main__":
     print(
-        "Choose which formula to use: 1 for square, 2 for tri, 3 for lazy caterer, 4 for magic squares"
+        "Choose which formula to use: 1 for square, 2 for tri, 3 for lazy caterer, 4 for magic squares, 5 for calculate hypotenuse,, 6 for volume of sphere, 7 for celcius to farheinheit"
     )
     input_formula = int(input())
     print(
